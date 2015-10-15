058bd064f1f9e130c6ff6634c10773ac0f6f21e4
JavaFXpert <james@j1w.xyz>
2015-10-15 02:14:19 -0400

    Update JFugue JAR and associated functionality

--- src/main/java/com/culturedear/chord/ChordAnalyzerController.java
+++ src/main/java/com/culturedear/chord/ChordAnalyzerController.java
@@ -1,9 +1,14 @@
 package com.culturedear.chord;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import org.jfugue.theory.Chord;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 /**
  * Created by jamesweaver on 10/13/15.
@@ -12,20 +17,27 @@ import org.springframework.web.bind.annotation.RestController;
 public class ChordAnalyzerController {
   private MusicChord musicChord;
   @RequestMapping("/analyze")
-  public MusicChord identifyChordByNotes(@RequestParam(value = "notes") String notes) {
+  public ResponseEntity<Object> identifyChordByNotes(@RequestParam(value = "notes") String notes) {
     Chord jfugueChord = null;
+    musicChord = null;
     try {
       jfugueChord = Chord.fromNotes(notes);
-      musicChord = new MusicChord(jfugueChord.toString(),
-                                  jfugueChord.getRoot().toString(),
+      musicChord = new MusicChord(jfugueChord.getRoot().toString(),
+                                  jfugueChord.getChordType(),
+                                  jfugueChord.getBassNote().toString(),
                                   jfugueChord.getInversion(),
                                   jfugueChord.isMajor(),
-                                  jfugueChord.isMinor());
+                                  jfugueChord.isMinor(),
+                                  jfugueChord.toString());
     }
     catch (Exception e) {
-      System.out.println("Exception encountered: " + e);
-      musicChord = new MusicChord("", "", 0, false, false);
+      System.out.println("Exception encountered in ChordAnalyzerController#identifyChordByNotes: " + e);
     }
-    return musicChord;
+    //musicChord = new MusicChord("", "", 0, false, false);
+    return Optional.ofNullable(musicChord)
+        .map(mc -> new ResponseEntity<>((Object)mc, HttpStatus.OK))
+        .orElse(new ResponseEntity<>("Could not analyze the chord", HttpStatus.INTERNAL_SERVER_ERROR));
+
   }
 }
+
--- src/main/java/com/culturedear/chord/MusicChord.java
+++ src/main/java/com/culturedear/chord/MusicChord.java
@@ -1,29 +1,39 @@
 package com.culturedear.chord;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
 /**
  * Created by jamesweaver on 10/13/15.
  */
 public class MusicChord {
-  private final String name;
   private final String root;
+  private final String chordType;
+  private final String bassNote;
   private final int inversion;
   private final boolean isMajor;
   private final boolean isMinor;
+  private final String name;
 
-  public MusicChord(String name, String root, int inversion, boolean isMajor, boolean isMinor) {
-    this.name = name;
+  public MusicChord(String root, String chordType, String bassNote, int inversion, boolean isMajor, boolean isMinor, String name) {
     this.root = root;
+    this.chordType = chordType;
+    this.bassNote = bassNote;
     this.inversion = inversion;
     this.isMajor = isMajor;
     this.isMinor = isMinor;
-  }
-
-  public String getName() {
-    return name;
+    this.name = name;
   }
 
   public String getRoot() {
     return root;
+  }
+
+  public String getChordType() {
+    return chordType;
+  }
+
+  public String getBassNote() {
+    return bassNote;
   }
 
   public int getInversion() {
@@ -36,5 +46,9 @@ public class MusicChord {
 
   public boolean isMinor() {
     return isMinor;
+  }
+
+  public String getName() {
+    return name;
   }
 }
